d3a7947c3edd5abde90b0e5c0ae7d3178b454a53
srisvk055 <82989922+srisvk055@users.noreply.github.com>
2022-01-02 23:24:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import os
 from flask import Flask
+from waitress import serve
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8080)
